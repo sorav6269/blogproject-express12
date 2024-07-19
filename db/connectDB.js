12821edaebf4b78623252203e57cbc637afd1109
Abhi9893 <patsariya.abhi@gmail.com>
2024-07-19 17:38:58 +0530

    hello

--- db/connectDB.js
+++ db/connectDB.js
@@ -1,9 +1,10 @@
 const mongoose = require("mongoose")
 
 const localURL = "mongodb://127.0.0.1:27017/blogprojectexpress"
+const liveUrl ="mongodb+srv://pn123:pn123@cluster0.5ud8ykv.mongodb.net/blogproject?retryWrites=true&w=majority&appName=Cluster0"
 
 const connect = () => {
-    return mongoose.connect(localURL)
+    return mongoose.connect(liveUrl)
         .then(() => {
             console.log('connect success')
         }).catch((error) => {
