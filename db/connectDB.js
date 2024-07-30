5491d76615c0462a21647770ed26ace7ca45e2ca
sorav6269 <soravrathor786@gmail.com>
2024-07-30 15:57:00 +0530

    h

--- db/connectDB.js
+++ db/connectDB.js
@@ -4,7 +4,7 @@ const localURL = "mongodb://127.0.0.1:27017/blogprojectexpress"
 const liveUrl= "mongodb+srv://soravrathor786:sorav1@cluster0.tkmjxwk.mongodb.net/blogproject?retryWrites=true&w=majority&appName=Cluster0"
 
 const connect = () => {
-    return mongoose.connect(localURL)
+    return mongoose.connect(liveUrl)
         .then(() => {
             console.log('connect success')
         }).catch((error) => {
